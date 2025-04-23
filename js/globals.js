e8dae4997154c1694313b92b446782e39a00eccd
Michael Ellis <michael.f.ellis@gmail.com>
2025-04-23 12:37:01 -0400

    The Great Simplification. Removed all source code except index.html

--- js/globals.js
+++
@@ -1,7 +0,0 @@
-// DOM elements
-const apiKeyInput = document.getElementById('api-key');
-const saveKeyBtn = document.getElementById('save-key');
-const modelSelector = document.getElementById('model-selector');
-const sendButton = document.getElementById('send-button');
-const apiProviderSelector = document.getElementById('api-provider');
-const temperatureInput = document.getElementById('temperature-input');
